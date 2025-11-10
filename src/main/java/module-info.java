51f8891d3f651d328ff8b5caa667b340a33d5d6a
Shohana Akter Rabina <shohana592059@gmail.com>
2025-11-10 21:42:54 +0600

    Initial commit project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.rabina_2207057_cvbuilder {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.rabina_2207057_cvbuilder to javafx.fxml;
+    exports com.example.rabina_2207057_cvbuilder;
+}
