cc5c54c9b78964e84058ce3dcf9825849714561a
Shohana Akter Rabina <shohana592059@gmail.com>
2025-11-14 21:57:58 +0600

    Add HomePage

---
+++ src/main/java/com/example/rabina_2207057_cvbuilder/front_page_controller.java
@@ -0,0 +1,13 @@
+package com.example.rabina_2207057_cvbuilder;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class front_page_controller {
+
+    @FXML
+    void goToNextPage(ActionEvent event) {
+
+    }
+
+}
