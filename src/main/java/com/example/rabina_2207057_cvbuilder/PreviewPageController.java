02e27831fe2be3870292ac95c3ba0bbbbdfbe575
Shohana Akter Rabina <shohana592059@gmail.com>
2025-11-15 17:46:13 +0600

    main CV page

---
+++ src/main/java/com/example/rabina_2207057_cvbuilder/PreviewPageController.java
@@ -0,0 +1,8 @@
+package com.example.rabina_2207057_cvbuilder;
+
+import javafx.event.ActionEvent;
+
+public class PreviewPageController {
+    public void addToTable(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/example/rabina_2207057_cvbuilder/page2_controller.java
+++ src/main/java/com/example/rabina_2207057_cvbuilder/page2_controller.java
@@ -1,24 +1,100 @@
 package com.example.rabina_2207057_cvbuilder;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseDragEvent;
-import javafx.scene.input.MouseEvent;
 
 public class page2_controller {
 
     @FXML
-    private TextField fullNameField;
+    private TextField txtAddress;
 
     @FXML
-    void maximum15character(MouseDragEvent event) {
-
-    }
+    private TextField txtB1;
 
     @FXML
-    void print(MouseEvent event) {
+    private TextField txtB2;
 
-    }
+    @FXML
+    private DatePicker txtBirth;
+
+    @FXML
+    private TextField txtCGPA1;
+
+    @FXML
+    private TextField txtCGPA2;
+
+    @FXML
+    private TextField txtD1;
+
+    @FXML
+    private TextField txtD2;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtExam1;
+
+    @FXML
+    private TextField txtExam2;
+
+    @FXML
+    private TextField txtExam3;
+
+    @FXML
+    private TextField txtExam4;
+
+    @FXML
+    private TextArea txtExperience;
+
+    @FXML
+    private TextField txtFather;
+
+    @FXML
+    private TextField txtFullName;
+
+    @FXML
+    private TextField txtG1;
+
+    @FXML
+    private TextField txtG2;
+
+    @FXML
+    private TextField txtGPA1;
+
+    @FXML
+    private TextField txtGPA2;
+
+    @FXML
+    private TextField txtMother;
+
+    @FXML
+    private TextField txtNumber;
+
+    @FXML
+    private TextArea txtProject;
+
+    @FXML
+    private TextArea txtSkills;
+
+    @FXML
+    private TextField txtU1;
+
+    @FXML
+    private TextField txtU2;
+
+    @FXML
+    private TextField txtY1;
+
+    @FXML
+    private TextField txtY2;
+
+    @FXML
+    private TextField txtY3;
+
+    @FXML
+    private TextField txtY4;
 
 }
-
