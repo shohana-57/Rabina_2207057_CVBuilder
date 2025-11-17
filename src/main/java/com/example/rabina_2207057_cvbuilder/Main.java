5f46c123e39e9d86e8bcba33dacf5c1d72fc2fd6
Shohana Akter Rabina <shohana592059@gmail.com>
2025-11-17 21:40:55 +0600

    changing running main filename

--- src/main/java/com/example/rabina_2207057_cvbuilder/Main.java
+++ src/main/java/com/example/rabina_2207057_cvbuilder/Main.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("front_page.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("front_page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 300, 250);
         stage.setTitle("HomePage");
         stage.setScene(scene);
