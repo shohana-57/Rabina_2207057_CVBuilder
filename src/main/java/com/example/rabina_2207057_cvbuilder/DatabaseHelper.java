a487c094d75263805e4119c77cc9a7c4032c20a2
Shohana Akter Rabina <shohana592059@gmail.com>
2025-11-25 03:35:48 +0600

    Table creation

--- src/main/java/com/example/rabina_2207057_cvbuilder/DatabaseHelper.java
+++ src/main/java/com/example/rabina_2207057_cvbuilder/DatabaseHelper.java
@@ -1,11 +1,40 @@
 package com.example.rabina_2207057_cvbuilder;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DatabaseHelper {
-    private static final String DB_URL = "jdbc:sqlite:sample.db";
+    private static final String DB_URL = "jdbc:sqlite:CV_Database.db";
     private static Connection connection;
+
+    private static final String CREATE_TABLE_SQL =
+            "CREATE TABLE IF NOT EXISTS sample_nodes (" +
+                    "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                    "json_data TEXT NOT NULL" +
+                    ");";
+
+    public static void initDatabase() throws SQLException {
+        try {
+            if(connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(DB_URL);
+                createTable();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void createTable() {
+        try {
+            PreparedStatement statement = connection.prepareStatement(CREATE_TABLE_SQL);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static List<CVNode> getAllSamples() {
         return List.of();
