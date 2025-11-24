db51ff9bb3bb7f72c9c45dce6a8e68306a0642b4
Shohana Akter Rabina <shohana592059@gmail.com>
2025-11-25 02:17:39 +0600

    sql class

--- src/main/java/com/example/rabina_2207057_cvbuilder/CVRepository.java
+++ src/main/java/com/example/rabina_2207057_cvbuilder/CVRepository.java
@@ -1,7 +1,24 @@
 package com.example.rabina_2207057_cvbuilder;
 
+import javafx.application.Platform;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
 public class CVRepository {
-    public void getAllAsync(Object o, Object o1) {
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    public void getAllAsync(Consumer<List<CVNode>> onSuccess, Consumer<Throwable> onError) {
+        executor.submit(() ->{
+            try {
+
+                List<CVNode> list = DatabaseHelper.getAllSamples();
+                Platform.runLater(() -> onSuccess.accept(list));
+            } catch (Throwable ex) {
+                Platform.runLater(() -> onError.accept(ex));
+            }
+        });
 
     }
 
