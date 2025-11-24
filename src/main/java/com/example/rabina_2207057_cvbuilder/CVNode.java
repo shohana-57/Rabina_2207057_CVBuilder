3ad5c884b2db74f763d7bf09cc37fd0b1a3a13ae
Shohana Akter Rabina <shohana592059@gmail.com>
2025-11-24 23:41:31 +0600

    handling error

--- src/main/java/com/example/rabina_2207057_cvbuilder/CVNode.java
+++ src/main/java/com/example/rabina_2207057_cvbuilder/CVNode.java
@@ -4,27 +4,35 @@ import java.time.LocalDate;
 
 public class CVNode {
     public String getFullName() {
+        return "";
     }
 
     public String getFatherName() {
+        return "";
     }
 
     public String getMotherName() {
+        return "";
     }
 
     public String getEmail() {
+        return "";
     }
 
     public String getAddress() {
+        return "";
     }
 
     public String getContactNumber() {
+        return "";
     }
 
     public LocalDate getDateofBirth() {
+        return null;
     }
 
     public String getExam1() {
+        return "";
     }
 
     public String getB1() {
@@ -46,5 +54,50 @@ public class CVNode {
     }
 
     public String getG2() {
+    }
+
+    public String getGPA2() {
+    }
+
+    public String getY2() {
+    }
+
+    public String getExam3() {
+    }
+
+    public String getU1() {
+    }
+
+    public String getD1() {
+    }
+
+    public String getCGPA1() {
+    }
+
+    public String getY3() {
+    }
+
+    public String getExam4() {
+    }
+
+    public String getU2() {
+    }
+
+    public String getD2() {
+    }
+
+    public String getCGPA2() {
+    }
+
+    public String getY4() {
+    }
+
+    public String getSkills() {
+    }
+
+    public String getExperience() {
+    }
+
+    public String getProjects() {
     }
 }
